5863fcd0e95b440168902e04f92202b1473cf36d
DedPerded <lightman76@yandex.ru>
2022-12-12 23:22:03 +0300

    added some tests

--- crudapp/app_test.go
+++ crudapp/app_test.go
@@ -119,3 +119,141 @@ func TestGet(t *testing.T) {
 	}
 
 }
+
+func TestList(t *testing.T) {
+
+	store := NoteStorage{
+		NoteList: []*Note{
+			{
+				ID:        0,
+				Text:      "first note",
+				CreatedAt: time.Now().Add(-48 * time.Hour).Round(time.Hour),
+				UpdatedAt: time.Now().Round(time.Hour),
+			},
+			{
+				ID:        1,
+				Text:      "second note",
+				CreatedAt: time.Now().Add(-24 * time.Hour).Round(time.Hour),
+				UpdatedAt: time.Now().Add(-4 * time.Hour).Round(time.Hour),
+			},
+		},
+	}
+
+	port := "8080"
+	req := httptest.NewRequest("GET", fmt.Sprintf("localhost:%s/note", port), nil)
+
+	w := httptest.NewRecorder()
+
+	store.List(w, req)
+
+	if w.Code != 200 {
+		t.Errorf("Wrong code\nExpected: 200\nGot: %d", w.Code)
+	}
+
+	resp := w.Result()
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read body: %v", err)
+	}
+
+	recievedNotes := []*Note{}
+	errMarshal := json.Unmarshal(responseBody, &recievedNotes)
+	if errMarshal != nil {
+		t.Errorf("Err unmarshalling: %v", errMarshal)
+	}
+
+	expectedNotes := store.NoteList
+
+	if !reflect.DeepEqual(recievedNotes, expectedNotes) {
+		t.Errorf("response isnt correct\nWanted: %v\nGot: %v", expectedNotes, recievedNotes)
+	}
+
+}
+
+func TestUpdate(t *testing.T) {
+	store := NoteStorage{
+		NoteList: []*Note{
+			{
+				ID:        0,
+				Text:      "first task",
+				CreatedAt: time.Now().Round(time.Second),
+				UpdatedAt: time.Now().Round(time.Second),
+			},
+		},
+	}
+
+	newNote := map[string]string{
+		"text": "changed task",
+	}
+
+	reqBody, err := json.Marshal(newNote)
+	if err != nil {
+		t.Errorf("Err marshalling: %v", err)
+	}
+
+	port := "8080"
+	req := httptest.NewRequest("PUT", fmt.Sprintf("localhost:%s/note/0", port), bytes.NewReader(reqBody))
+	vars := map[string]string{
+		"id": "0",
+	}
+	req = mux.SetURLVars(req, vars)
+
+	w := httptest.NewRecorder()
+
+	store.Update(w, req)
+
+	if w.Code != 200 {
+		t.Errorf("Wrong code\nExpected: 200\nGot: %d", w.Code)
+	}
+
+	resp := w.Result()
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read body: %v", err)
+	}
+
+	if string(responseBody) != "success" {
+		t.Errorf("response isnt correct\nWanted: succsess\nGot: %s", string(responseBody))
+	}
+}
+
+func TestDelete(t *testing.T) {
+	store := NoteStorage{
+		NoteList: []*Note{
+			{
+				ID:        0,
+				Text:      "first task",
+				CreatedAt: time.Now().Round(time.Second),
+				UpdatedAt: time.Now().Round(time.Second),
+			},
+		},
+	}
+
+	port := "8080"
+	req := httptest.NewRequest("DELETE", fmt.Sprintf("localhost:%s/note/0", port), nil)
+	vars := map[string]string{
+		"id": "0",
+	}
+	req = mux.SetURLVars(req, vars)
+
+	w := httptest.NewRecorder()
+
+	store.Delete(w, req)
+
+	if w.Code != 200 {
+		t.Errorf("Wrong code\nExpected: 200\nGot: %d", w.Code)
+	}
+
+	resp := w.Result()
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read body: %v", err)
+	}
+
+	if string(responseBody) != "success" {
+		t.Errorf("response isnt correct\nWanted: succsess\nGot: %s", string(responseBody))
+	}
+}
